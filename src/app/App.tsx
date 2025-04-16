9d43ecc47fc9e06b3b1fbadba00d417d1f4c0491
Pavel Soxanevich <soxanevich@mail.ru>
2025-04-16 17:02:11 +0300

    add antd styled-components and sb

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import 'antd/dist/reset.css';
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Ant Design</h1>
+      <Button type="default">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
